c46f8976e4954a3781f2fed8eeac8c07d6c7a548
daniel <danielderu@gmail.com>
2022-02-03 08:27:52 +0200

    added the work page layout

--- pages/skills.js
+++ pages/skills.js
@@ -1,7 +1,16 @@
+import styles from "../styles/Skills.module.css"
+
 const skills = () => {
+  const LANGUAGES = ["HTML", "CSS", "JavaScript", "PHP", "Python", "C++", "TypeScript"]
   return (
-    <div>
-        <h1>Skills Page</h1>
+    <div className={styles.skills}>
+        <div className="Languages">
+          <div>Languages</div>
+          
+        </div>
+        <div className="Frameworks Other">
+
+        </div>
     </div>
   );
 };
--- pages/work.js
+++ pages/work.js
@@ -1,7 +1,24 @@
+import styles from "../styles/Work.module.css"
+
 const work = () => {
   return(
-    <div>
-        <h1>Work Page</h1>
+    <div className={styles.work}>
+        <section>
+          <div></div>
+          <div></div>
+          <div></div>
+        </section>
+        <section>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </section>
+        <section>
+          <div></div>
+          <div></div>
+          <div></div>
+        </section>
     </div>
   );
 };
