d0558bb2a60b274db00e151e9189079b706bb2fc
daniel <danielderu@gmail.com>
2022-02-11 08:25:17 +0200

    turned projects into motion components. using a 2D array to create projects dynamically in the vDOM

--- pages/work.js
+++ pages/work.js
@@ -1,24 +1,44 @@
+import { motion } from "framer-motion";
 import { Work } from "./styled/work.styled";
+import { useState } from "react"
+
+const projectsArray = [
+  [1,2,3],
+  [4,5,6,7],
+  [8,9,10]
+]
+let i = 0
 
 const work = () => {
+
+  const projectVariant = {
+    hidden: {
+      x: "100vw"
+    },
+    visible: index => ({
+      x: 0,
+      transition: {
+        delay: index + 1,
+        ease: "easeOut"
+      }
+    })
+  }
   return(
     <Work>
-        <section>
-          <div></div>
-          <div></div>
-          <div></div>
-        </section>
-        <section>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </section>
-        <section>
-          <div></div>
-          <div></div>
-          <div></div>
-        </section>
+        {projectsArray.map((projectGroup) => 
+          <motion.section>
+            {projectGroup.map((project, index) => 
+              <motion.div
+                variants={projectVariant}
+                initial="hidden"
+                animate="visible"
+                custom={index}
+              >
+
+              </motion.div>
+            )}
+          </motion.section>
+        ) }
     </Work>
   );
 };
